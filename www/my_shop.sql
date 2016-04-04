65d7f23703c345219af1219b851a72b5c0c74959
Алексей <alx.mashkovich@gmail.com>
2016-04-04 22:47:20 +0300

    add sql backup

---
+++ www/my_shop.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 04 2016 г., 23:45
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `my_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `access` tinyint(1) NOT NULL DEFAULT '1',
+  `amount` smallint(5) unsigned NOT NULL,
+  `price` bigint(20) unsigned NOT NULL,
+  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `image` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `product`
+--
+
+INSERT INTO `product` (`id`, `user_id`, `name`, `description`, `access`, `amount`, `price`, `published`, `image`) VALUES
+(1, 1, 'Сандаль', 'Сандаль правый', 1, 1, 25000, '2016-04-03 09:40:59', 'http://cs419429.vk.me/v419429659/38ff/odYxDvjQJqE.jpg'),
+(2, 1, 'Штанина', 'Штанина за 20 гривен', 1, 5, 20, '2016-04-03 09:40:59', 'http://i1353.photobucket.com/albums/q672/nakasun/ralph-lauren--p_n_35297_B_zpsf8480593.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(254) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `second_name` varchar(50) DEFAULT NULL,
+  `patronymic` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email_2` (`email`),
+  KEY `email_3` (`email`),
+  KEY `email_4` (`email`),
+  KEY `password_2` (`password`),
+  KEY `password_3` (`password`),
+  KEY `email_password` (`email`,`password`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `first_name`, `second_name`, `patronymic`, `phone`) VALUES
+(1, 'aleshacasha@gmail.com', '1234', 'alex', 'mashkovich', 'anatolevich', '2545417');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
